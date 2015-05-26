84516e94705e83142cc8a30ce11d3d589684d73b
peterjacobson <peterjacobson.nz@gmail.com>
2015-05-26 17:24:44 +1200

    add product#create tests

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -37,5 +37,65 @@ RSpec.describe ProductsController, type: :controller do
 		it { should respond_with(200) }
 		it { should render_template(:new) }
 	end
+
+	describe '#create' do
+		
+		context "if valid params" do
+			before do
+				@product_params = attributes_for(:product)
+				post :create, { product: @product_params }
+			end
+
+			it { should respond_with(302) }
+			it "should redirect to the new product's page" do
+				product = Product.find_by(@product_params)
+				expect(response).to redirect_to("/products/#{product.id}")
+			end
+		end
+
+		context "if invalid params" do
+			before do
+				@invalid_params = {story: 'no name'}
+				post :create, { product: @invalid_params}
+			end
+
+			it { should respond_with(400) }			
+			it { should render_template(:new) }
+			it "should not create a new product" do
+				expect(Product.where(@invalid_params)).to be_nil
+			end
+		end
+
+	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
