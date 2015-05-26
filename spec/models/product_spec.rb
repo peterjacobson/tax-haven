89562bbeef1e15b3f2fb69312bde8fcfdc5206b6
peterjacobson <peterjacobson.nz@gmail.com>
2015-05-27 10:58:45 +1200

    devise working, figuring out tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
 	let(:product) {create(:product)}
+	let(:transport_tax) {create(:transport_tax)}
+	let(:landfill_tax) {create(:landfill_tax)}
+	subject { 
+		product.taxes << transport_tax
+		product.taxes << landfill_tax
+	}
 
 	describe "fields" do
 		it { should have_db_column(:name).of_type(:string) }
@@ -9,5 +15,10 @@ RSpec.describe Product, type: :model do
 
 	describe "validations" do
 		it { should validate_presence_of(:name)}
+	end
+
+	describe "relations" do
+		subject
+		# it { }
 	end
 end
